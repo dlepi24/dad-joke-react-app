aef7969d2c207e74e5d593a45a0dc3709a959e16
Dustin <dustin@leapit.us>
2023-08-25 22:01:03 -0500

    Eh, it works?

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import DadJoke from './DadJoke';
 import './App.css';
 import logo from './logo.png';
 
 function App() {
+  const jokeContainerRef = useRef(null);
+
+  const scrollToRef = () => {
+    if (jokeContainerRef.current) {
+      jokeContainerRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -13,14 +21,13 @@ function App() {
       <div className="Logo-container">
         <img src={logo} className="App-logo" alt="logo" />
       </div>
-      <DadJoke />
+      <DadJoke scrollToRef={scrollToRef} jokeContainerRef={jokeContainerRef} />
       <footer>
         <a href="https://info.leapit.us" target="_blank" rel="noopener noreferrer" className="navigation-link">
           <span role="img" aria-label="Hand pointing right">👉</span>
           Check out my GitBook to see how this was built! 
           <span role="img" aria-label="Hand pointing left">👈</span>
         </a>
-
       </footer>
     </div>
   );
--- src/DadJoke.jsx
+++ src/DadJoke.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import './DadJoke.css';
 
-function DadJoke() {
+function DadJoke({ scrollToRef, jokeContainerRef }) {
   const [joke, setJoke] = useState('');
+  const [showJoke, setShowJoke] = useState(false);
 
   const fetchJoke = async () => {
     const response = await fetch('https://icanhazdadjoke.com/', {
@@ -12,20 +13,25 @@ function DadJoke() {
     });
     const data = await response.json();
     setJoke(data.joke);
+    setShowJoke(true);
   };
 
+  // Scroll to the joke container when the joke updates
   useEffect(() => {
-    fetchJoke();
-  }, []);
+    if (showJoke && jokeContainerRef.current) {
+      jokeContainerRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, [joke, showJoke, jokeContainerRef]);
 
   return (
     <div className="DadJoke">
-      <div className="Joke-container">
-        <p><strong>{joke}</strong></p>
-      </div>
-      <button className="Joke-button" onClick={fetchJoke}><strong>
-      Get Another Joke
-      </strong>
+      {showJoke && (
+        <div className="Joke-container" ref={jokeContainerRef}>
+          <p><strong>{joke}</strong></p>
+        </div>
+      )}
+      <button className="Joke-button" onClick={fetchJoke}>
+        <strong>{joke ? 'Another one?' : 'Click Here!'}</strong>
       </button>
     </div>
   );
